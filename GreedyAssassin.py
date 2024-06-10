0201ba583102576d16a000735203f49c0c4c4d89
Efi <belminoruc1999@gmail.com>
2024-06-10 16:38:50 +0200

    Feat: IMplemented the weighted system into the first part of the algorithm

--- GreedyAssassin.py
+++ GreedyAssassin.py
@@ -11,33 +11,48 @@ def mutually_exclusive(arr1, arr2):
     return not bool(set1.intersection(set2))
 
 
-def compute_link_failures(G):
+def compute_link_failures(G, minWeight):
 
     # Step 1: Compute link failure scenarios
     link_failures = []
 
     # Step 2: Check for disjoint spanning trees
-    iterator = nx.algorithms.tree.mst.SpanningTreeIterator(G, weight='weight')
+    iterator1 = nx.algorithms.tree.mst.SpanningTreeIterator(G, weight='weight')
+    iterator2 = nx.algorithms.tree.mst.SpanningTreeIterator(G, weight='weight')
     # Get the first two spanning trees
-    iter(iterator)
-    tree1 = next(iterator)
-    tree2 = tree1
-    test= mutually_exclusive(tree1.edges, tree2.edges)
-    while test==False:
+    iter(iterator1)
+    sumWeight1=0
+    sumWeight2=0
+    test= False
+    while test==False or sumWeight1 < minWeight or sumWeight2 < minWeight:
+        test=False
         try:
-            tree2 = next(iterator)
-            test = mutually_exclusive(tree1.edges, tree2.edges)
+            tree1 = next(iterator1)
+            print("################################################################")
+            #how can we reset the iterator?
+            iterator2 = nx.algorithms.tree.mst.SpanningTreeIterator(G, weight='weight')
+            iter(iterator2)
+            while test==False:
+                try:
+                    tree2 = next(iterator2)
+                    print("Tree1:  ")
+                    print(tree1.edges)
+                    print("Tree2: ")
+                    print(tree2.edges)
+                    sumWeight1 = sum(tree1.edges[u, v]['weight'] for u, v in tree1.edges())
+                    sumWeight2 = sum(tree2.edges[u, v]['weight'] for u, v in tree2.edges())
+                    test = mutually_exclusive(tree1.edges, tree2.edges)
+                    print(test)
+                    print("----------------------------------------------------------------")
+                except:
+                    break
         except:
-            break
-    if(test==True):
-        T1 = nx.maximum_spanning_tree(G)
-        G.remove_edges_from(T1.edges)
-        T2 = nx.maximum_spanning_tree(G)
-        G.add_edges_from(T1.edges)
+             break
+    if(test==True and sumWeight1 >= minWeight and sumWeight2 >= minWeight):
 
         # Step 3: Fail links not in T1 and T2
-        link_failures.append(list(set(G.edges) - set(T1.edges)))
-        link_failures.append(list(set(G.edges) - set(T2.edges)))
+        link_failures.append(list(set(G.edges) - set(tree1.edges)))
+        link_failures.append(list(set(G.edges) - set(tree2.edges)))
     else:
 
         # Step 4: No disjoint spanning trees
@@ -63,10 +78,10 @@ def compute_link_failures(G):
 
             # Step 10: Set weights of failed links to 0
             for e in failed_links:
-                edge_weights[e] = edge_weights[e] * (-1)
+                edge_weights[e] = 0
 
             # Step 11: Check termination condition
-            if lambda_sum == 0 or all(weight <=0 for weight in edge_weights.values()):
+            if lambda_sum == 0 or all(weight ==0 for weight in edge_weights.values()):
                 break
     return link_failures
 
@@ -74,19 +89,20 @@ def compute_link_failures(G):
 
 
 #G = nx.petersen_graph()
-""""""
-G= nx.Graph();
-G.add_edge("1", "2", weight=4)
-G.add_edge("2", "3", weight=2)
-G.add_edge("3", "4", weight=3)
-G.add_edge("4", "1", weight=3)
-#G.add_edge("1", "3", weight=4)
 
+G= nx.Graph();
+G.add_edge("1", "2", weight=10)
+G.add_edge("2", "3", weight=0.1)
+G.add_edge("3", "4", weight=0.1)
+G.add_edge("4", "1", weight=12)
+G.add_edge("3", "1", weight=11)
+G.add_edge("4", "2", weight=0.1)
+#G.add_edge("1", "3", weight=4)
+""""""
+min=10
 nx.draw(G, with_labels=True, font_weight='bold')
-arr1= [('1', '4'), ('2', '3'), ('3', '4')]
-arr2= [('1', '4'), ('1', '2'), ('2', '3')]
 #Run program and show results
-link_failure_scenarios = compute_link_failures(G)
+link_failure_scenarios = compute_link_failures(G, min)
 print(link_failure_scenarios)
-#plt.show()
+plt.show()
 
